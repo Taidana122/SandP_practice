8c027a53207104b2d95d05f0a4818ef0003b96ae
Taidana122 <pavel.pikaper@mail.ru>
2025-07-04 00:06:21 +0300

    task_12.py

--- task_12.py
+++ task_12.py
@@ -1,12 +1,7 @@
-#Создайте класс JellyBean, расширяющий класс Dessert (из Упражнения 11) новым
-#геттером и сеттером для атрибута flavor (все параметры являются не
-#обязательными). Измените метод is_delicious таким образом, чтобы он возвращал
-#false только в тех случаях, когда flavor равняется «black licorice».
-
 class Dessert:
-    def __init__(self, name=None, calories=0):
-        self.name = name
-        self.calories = calories   
+    def __init__(self, name='', calories=0):
+        self._name = name
+        self._calories = calories   
     
     def is_healthy(self):
         return self.calories < 200
@@ -16,35 +11,37 @@ class Dessert:
     
     @property #превращает метод name в геттер
     def name(self):
-        return self.name #Позволяет обращаться к name, как к свойству, а не ф-ции
+        return self._name #Позволяет обращаться к name, как к свойству, а не ф-ции
     
     @property #calories в геттер
     def calories(self):
-        return self.calories
+        return self._calories
     
     @name.setter #сеттер для name
     def name(self, value):
-        self.value = value
+        self._name = value
 
     @calories.setter #сеттер для name
     def calories(self, value):
-        self.value = value
+        self._calories = value
 
 class JellyBean:
     def __init__(self, name=None, calories=0, flavor=None):
         super().__init__(name, calories)
-        self.flavor=flavor
+        self._flavor=flavor
     
     @property
     def flavor(self):
-        return self.flavor
+        return self._flavor
 
     @flavor.setter
     def flavor(self, value):
-        self.value = value
+        self._flavor = value
 
     def is_delicious(self):
-        if self.flavor != 'black licorice':
+        if self._flavor != 'black licorice':
             return False
         else:
             return True
+    
+    
