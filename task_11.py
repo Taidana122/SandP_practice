97b7d0ed5dec800dfafca4c0ac638a187f9a71e6
Taidana122 <pavel.pikaper@mail.ru>
2025-07-04 00:02:58 +0300

    task_11.py

--- task_11.py
+++ task_11.py
@@ -4,9 +4,9 @@
 #200) и is_delicious (возвращает true для всех десертов). 
 
 class Dessert:
-    def __init__(self, name=None, calories=0):
-        self.name = name
-        self.calories = calories   
+    def __init__(self, name='', calories=0):
+        self._name = name
+        self._calories = calories   
     
     def is_healthy(self):
         return self.calories < 200
@@ -16,18 +16,18 @@ class Dessert:
     
     @property #превращает метод name в геттер
     def name(self):
-        return self.name #Позволяет обращаться к name, как к свойству, а не ф-ции
+        return self._name #Позволяет обращаться к name, как к свойству, а не ф-ции
     
     @property #calories в геттер
     def calories(self):
-        return self.calories
+        return self._calories
     
     @name.setter #сеттер для name
     def name(self, value):
-        self.value = value
+        self._name = value
 
     @calories.setter #сеттер для name
     def calories(self, value):
-        self.value = value
+        self._calories = value
     
     
