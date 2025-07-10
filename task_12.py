a119da775c2a511c41e28176c1423aea6a64fe5d
Taidana122 <pavel.pikaper@mail.ru>
2025-07-11 00:33:59 +0300

    task_12.py

--- task_12.py
+++ task_12.py
@@ -4,42 +4,46 @@ class Dessert:
         self._calories = calories   
     
     def is_healthy(self):
-        return self.calories < 200
+        return self._calories < 200
 
     def is_delicious(self):
         return True
     
-    @property #превращает метод name в геттер
+    @property
     def name(self):
-        return self._name #Позволяет обращаться к name, как к свойству, а не ф-ции
+        return self._name
     
-    @property #calories в геттер
+    @property
     def calories(self):
         return self._calories
     
-    @name.setter #сеттер для name
+    @name.setter
     def name(self, value):
         self._name = value
 
-    @calories.setter #сеттер для name
+    @calories.setter
     def calories(self, value):
-        self._calories = value
+        try:
+            self._calories = int(value)
+        except (ValueError, TypeError):
+            self._calories = 0
+
 
 class JellyBean:
     def __init__(self, name=None, calories=0, flavor=None):
         super().__init__(name, calories)
-        self._flavor=flavor
+        self.flavor=flavor
     
     @property
     def flavor(self):
-        return self._flavor
+        return self.flavor
 
     @flavor.setter
     def flavor(self, value):
-        self._flavor = value
+        self.value = value
 
     def is_delicious(self):
-        if self._flavor != 'black licorice':
+        if self.flavor != 'black licorice':
             return False
         else:
             return True
